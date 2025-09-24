2cb23a6b65faac3b68efa5cfcd966d1f41579dcc
wearable-advocate <hmos-dtse@proton.me>
2025-09-24 20:24:14 +0300

    Initialize project

---
+++ README.md
@@ -0,0 +1,47 @@
+> **Note:** To access all shared projects, get information about environment setup, and view other guides, please visit [Explore-In-HMOS-Wearable Index](https://github.com/Explore-In-HMOS-Wearable/hmos-index).
+
+# **AES Decryption Demo using HUKS with (Sport)Lite Wearable**
+**A HarmonyOS demo showcasing secure AES decryption using HUKS framework**
+
+# **Preview**
+<div style="display: flex; gap: 5px;">
+  <img src="./screenshots/preview_1.png" width="25%">
+</div>
+
+# **Use Cases**
+- Secure decryption of encrypted byte arrays using AES CTR 128 NoPadding
+- Cryptographic key import and secure session management via HUKS
+- Data conversion between `Uint8Array` and text formats
+
+# **Tech Stack**
+- **Languages**:  JavaScript (Huawei LiteWearable)
+- **Tools**: Huawei DevEco Studio IDE 5.1.1.840
+- **Libraries**: Huks
+
+
+# **Directory Structure**
+```
+entry/src/main/js/MainAbility
+├───i18n
+│       en-US.json
+│       zh-CN.json
+├───pages
+│       Index.js // Core cryptographic logic using HUKS 
+│       Index.hml // UI layout 
+│       Index.css // Styling
+└─── 
+```
+
+# Constraints and Restrictions
+## Supported Devices
+- Huawei Sport (Lite) Watch GT 4/5
+- Huawei Sport (Lite) GT4/5 Pro
+- Huawei Sport (Lite) Fit 3/4
+- Huawei Sport (Lite) D2
+- Huawei Sport (Lite) Ultimate
+
+# Documentation
+- [Huawei HUKS Overview](https://developer.huawei.com/consumer/en/doc/harmonyos-references/js-apis-huks)
+
+# **License (MIT)**
+**AES Decryption Demo** is distributed under the terms of the MIT License. See the [LICENSE](LICENSE) file for more information.
---
+++ entry/src/main/js/MainAbility/pages/index/index.js
@@ -0,0 +1,121 @@
+import huks from '@ohos.security.huks';
+
+export default {
+    data: {
+        decryptedText: '',
+        error: false,
+        errorMessage: ''
+    },
+    onInit() {
+        const encryptedData = new Uint8Array([18, 27, 146, 85, 10, 42, 47, 216, 60, 107, 89, 157]);
+        const key = new Uint8Array(16).fill(0xAA);
+        const iv = new Uint8Array(16).fill(0x55);
+
+        console.log("encryptedData " + encryptedData)
+        console.log("key " + key)
+        console.log("iv " + iv)
+
+        const decrypredData = this.aesCtrDecode("keyalias", key, encryptedData, iv)
+
+        console.log("decrypted text: " + this.byteArrayToString(decrypredData))
+        this.decryptedText = this.byteArrayToString(decrypredData)
+    },
+
+    aesCtrDecode(keyAlias, key, encryptedData, iv) {
+        let properties = new Array();
+        properties[0] = {
+            tag: huks.HuksTag.HUKS_TAG_ALGORITHM,
+            value: huks.HuksKeyAlg.HUKS_ALG_AES
+        }
+
+        properties[1] = {
+            tag: huks.HuksTag.HUKS_TAG_PURPOSE,
+            value: huks.HuksKeyPurpose.HUKS_KEY_PURPOSE_DECRYPT
+        }
+
+
+        properties[2] = {
+            tag: huks.HuksTag.HUKS_TAG_KEY_SIZE,
+            value: huks.HuksKeySize.HUKS_AES_KEY_SIZE_128
+        }
+
+        properties[3] = {
+            tag: huks.HuksTag.HUKS_TAG_PADDING,
+            value: huks.HuksKeyPadding.HUKS_PADDING_NONE
+        }
+
+        properties[4] = {
+            tag: huks.HuksTag.HUKS_TAG_BLOCK_MODE,
+            value: huks.HuksCipherMode.HUKS_MODE_CTR
+        }
+
+        let importOptions = {
+            properties: properties,
+            inData: key
+        }
+
+        let decryptedData;
+
+        huks.importKeyItem(keyAlias, importOptions, (err, data) => {
+            if (err) {
+                console.error("Key import key item error." + JSON.stringify(err))
+                this.errorMessage = JSON.stringify(err)
+                this.error = true
+                return
+            }
+
+            let sessionProperties = [...properties];
+            sessionProperties.push({
+                tag: huks.HuksTag.HUKS_TAG_IV,
+                value: iv
+            })
+
+            let initOptions = {
+                properties: sessionProperties,
+                inData: new Uint8Array(0)
+            }
+
+            huks.initSession(keyAlias, initOptions, (initErr, sessionData) => {
+                if (initErr) {
+                    console.error("Key init session err" + JSON.stringify(initErr))
+                    this.errorMessage = JSON.stringify(initErr)
+                    this.error = true
+                }
+
+                const handle = sessionData.handle
+                let finishOptions = {
+                    properties: sessionProperties,
+                    inData: encryptedData
+                }
+
+                huks.finishSession(handle, finishOptions, (finishErr, finishData) => {
+                    if (finishErr) {
+                        console.error("Decrypt err" + finishErr)
+                        this.errorMessage = JSON.stringify(finishErr)
+                        this.error = true
+                        return;
+                    }
+
+                    if (!finishData) {
+                        console.error("FinishData err" + finishData)
+                        this.errorMessage = JSON.stringify(finishData)
+                        this.error = true
+                        return;
+                    }
+                    this.error = false;
+                    decryptedData = finishData.outData;
+                })
+            })
+        })
+        console.log("decrypred Data Uint8Array:" + decryptedData);
+        return decryptedData;
+    },
+
+    byteArrayToString(byteArray) {
+        var string = '';
+        for (var i = 0; i < byteArray.length; i++) {
+            string = string + String.fromCharCode(byteArray[i])
+        }
+        return string
+    }
+}
